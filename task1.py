9decf635619f14d11aa2c853a09e0b438a945d14
DenisA777 <denisananyan333@gmail.com>
2025-03-26 10:36:02 +0200

    Add files via upload

--- task1.py
+++ task1.py
@@ -23,13 +23,16 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    pass
+    def __init__(self, value):
+        super().__init__(value)
+
 
 class Phone(Field):
     def __init__(self, value):
-        if not (value.isdigit() and len(value) == 10):
-            raise ValueError("Phone number must be 10 digits.")
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError('Phone number must be 10 digits.')
         super().__init__(value)
+            
 
 class Birthday(Field):
     def __init__(self, value):
@@ -53,10 +56,15 @@ class Record:
         self.phones.append(Phone(phone))
     
     def edit_phone(self, old_phone, new_phone):
-        for i in range(len(self.phones)):
-            if self.phones[i].value == old_phone:
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
                 self.phones[i] = Phone(new_phone)
                 return
+        raise ValueError('Phone number not found.')
+    
+    def find_phone(self, phone):
+        return next((p for p in self.phones if p.value == phone), None)
+
     
     def __str__(self):
         phones_str = ", ".join(p.value for p in self.phones)
@@ -67,8 +75,18 @@ class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
     
-    def search_by_name(self, name):
-        return self.data.get(name)
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise ValueError('No record.')
+
+    def __str__(self):
+        return '\n'.join(str(record) for record in self.data.values())
+
     
     def get_upcoming_birthdays(self):
         upcoming_birthdays = []
